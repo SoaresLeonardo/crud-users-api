6f9e9e101da249f7fa4f6c27923d0c6079f347f9
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-04-12 01:21:51 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello" });
+});
+
+app.listen(port, () => console.log(`Server is running in port ${port} :)`));
