3d82dffd7845b7374589c62460bcbff6b7d5e365
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-04-14 19:21:21 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { CreateUserController } from "./modules/users/useCases/CreateUser/CreateUserController";
-import { GetAllUsersController } from "./modules/users/useCases/GetAllUsers/GetAllUsersController";
+import createUserController from "./modules/users/useCases/CreateUser";
+import getAllUsersController from "./modules/users/useCases/GetAllUsers";
+import deleteUserController from "./modules/users/useCases/DeleteUser";
 
 const routes = Router();
 
-const createUserUseController = new CreateUserController();
-const getAllUsersController = new GetAllUsersController();
-
-routes.get("/", getAllUsersController.handle);
-routes.post("/", createUserUseController.handle);
+routes.get("/users", (req, res) => getAllUsersController.handle(req, res));
+routes.post("/users", (req, res) => createUserController.handle(req, res));
+routes.delete("/users/:id", (req, res) => deleteUserController.handle(req, res));
 
 export default routes;
