4c97693d4710573228be8b076a3163378a5d95ad
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-04-16 22:40:30 -0300

    moving the validations

--- src/modules/users/useCases/CreateUser/CreateUserController.ts
+++ src/modules/users/useCases/CreateUser/CreateUserController.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
+import { ApiError } from "../../../../shared/middleware/ApiError";
+import { validateEmail } from "../../validators/EmailValidator";
 
 export class CreateUserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
   async handle(req: Request, res: Response): Promise<Response> {
     const { name, email } = req.body;
+
+    if (!name) {
+      throw new ApiError("Name was not provided in the request", 400);
+    }
+
+    if (!email) {
+      throw new ApiError("E-mail was not provided in the request", 400);
+    }
+
+    if (!validateEmail(email)) {
+      throw new ApiError("Invalid email", 400);
+    }
 
     const response = await this.createUserUseCase.execute({ name, email });
 
--- src/modules/users/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/CreateUser/CreateUserUseCase.ts
@@ -3,12 +3,10 @@ import { prisma } from "../../../../shared/services/prisma";
 import { ApiError } from "../../../../shared/middleware/ApiError";
 import { ICreateUserDTO } from "../../dtos/CreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
-import { validateEmail } from "../../validators/EmailValidator";
 export class CreateUserUseCase {
   constructor(private readonly userRepository: IUsersRepository) {}
 
   async execute({ name, email }: ICreateUserDTO): Promise<User> {
-    // Looking for user
     const userAlreadyExists = await prisma.user.findUnique({
       where: {
         email,
@@ -19,20 +17,6 @@ export class CreateUserUseCase {
     if (userAlreadyExists) {
       throw new ApiError("User already exists", 422);
     }
-    
-    if (!name) {
-      throw new ApiError("Name was not provided in the request", 400);
-    }
-
-    if (!email) {
-      throw new ApiError("E-mail was not provided in the request", 400);
-    }
-
-    if (!validateEmail(email)) {
-      throw new ApiError("Invalid email", 400);
-    }
-
-
 
     // create user
     const user = await this.userRepository.create({
